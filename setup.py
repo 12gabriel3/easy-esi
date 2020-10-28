209bcc16756fed9aadd51042d0a37487760eee4d
12gabriel3 <aguiargab@gmail.com>
2020-10-28 08:32:25 -0300

    Added python 3.9 support

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     package_data={
         'easy_esi': ['py.typed'],
